7de9e8dd0e708d1210919e884c91d6fa683cd93b
Bryce Nadj <bryce.nadj@gmail.com>
2022-03-11 12:02:00 +1000

    Add project files.

---
+++ PerfectPoliciesApi/Entities/PerfectPoliciesContext.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PerfectPoliciesApi.Entities
+{
+    public class PerfectPoliciesContext : DbContext
+    {
+        public PerfectPoliciesContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<Question> Questions { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            builder.Entity<Question>().HasData(
+                new Question { questionId = 0, topic = "math", answer = "5", ansA = "1", ansB = "2", ansC = "3", ansD = "4", ansE = "5" }
+                );
+        }
+    }
+}
---
+++ PerfectPoliciesApi/Entities/Question.cs
@@ -0,0 +1,14 @@
+﻿namespace PerfectPoliciesApi.Entities
+{
+    public class Question
+    {
+        public int questionId { get; set; }
+        public string topic { get; set; }
+        public string answer { get; set; }
+        public string ansA { get; set; }
+        public string ansB { get; set; }
+        public string ansC { get; set; }
+        public string ansD { get; set; }
+        public string ansE { get; set; }
+    }
+}
