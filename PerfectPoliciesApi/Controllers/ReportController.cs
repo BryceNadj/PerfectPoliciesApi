02f99a9aba902f6900efb75d89af38697097eb61
Bryce Nadj <bryce.nadj@gmail.com>
2022-05-24 15:00:59 +1000

    Add option count for questions report

---
+++ PerfectPoliciesApi/Controllers/ReportController.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using PerfectPoliciesApi.DTO.ViewModels;
+using PerfectPoliciesApi.Entities;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PerfectPoliciesApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ReportController : ControllerBase
+    {
+        // Constructor injection to provide acess to database
+        private readonly PerfectPoliciesContext _context;
+
+        public ReportController(PerfectPoliciesContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet("OptionQuestionCount")]
+        public IActionResult OptionQuestionCount()
+        {
+            var questions = _context.Questions.Include(c => c.Options);
+
+            var questionOptionCountList = questions.Select(c => new OptionCountForQuestion
+            {
+                QuestionText = c.QuestionText,
+                OptionCount = c.Options.Count
+            }).ToList();
+
+            // Question   - Text
+            // - Option   - +1
+            // - Option   - +1
+            // - Option   - +1
+            // Question   - Text
+            // - Option   - +1
+            // - Option   - +1
+
+            return Ok(questionOptionCountList);
+        }
+    }
+}
---
+++ PerfectPoliciesApi/DTO/ViewModels/OptionCountForQuestion.cs
@@ -0,0 +1,9 @@
+﻿namespace PerfectPoliciesApi.DTO.ViewModels
+{
+    public class OptionCountForQuestion
+    {
+        public string QuestionText { get; set; }
+
+        public int OptionCount { get; set; }
+    }
+}
